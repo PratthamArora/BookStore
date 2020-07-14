a75e2503b6e663de2408971e4f7379c98307dce4
PratthamArora <arora.prattham@gmail.com>
2020-07-14 23:09:13 +0530

    setup mongodb

--- src/data/model/Book.kt
+++ src/data/model/Book.kt
@@ -1,8 +1,15 @@
 package com.pratthamarora.data.model
 
-data class Book(
-    var id: String,
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+
+class Book(
+    @field:BsonId var id: ObjectId?,
     var title: String,
     var author: String,
     var price: Float
-)
+) {
+
+    constructor() : this(null, "not_set", "not_set", 0.0f)
+
+}
